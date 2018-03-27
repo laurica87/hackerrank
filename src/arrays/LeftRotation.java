3b9db778c93c58b7bcf639d406b558da26e649ac
laura <laura@192.168.0.10>
2018-03-27 22:11:50 +0200

    Add empty LeftRotation

---
+++ src/arrays/LeftRotation.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class LeftRotation {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
+
